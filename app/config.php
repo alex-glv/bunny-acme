d61da275610b78132b5cb61390e379188a1a84d4
Aleksandr Guljajev <gulyayev.alex@gmail.com>
2016-07-17 22:16:12 +0200

    Added connection timetou

--- app/config.php
+++ app/config.php
@@ -9,31 +9,45 @@ use \Monolog\Handler\AmqpHandler;
 return array(
     "amqp-config" => function ($c) {
         $amqpCfg = new stdClass;
-        $amqpCfg->host = "10.0.2.2";
+        $amqpCfg->host = "rabbitmq";
         $amqpCfg->port = 5672;
         $amqpCfg->user = "guest";
         $amqpCfg->pass = "guest";
         $amqpCfg->exchange_name = "local.queue.events";
+        $amqpCfg->timeout = 5;
         return $amqpCfg;
     },
-    
+
     "amqp-connection" => function ($c) {
         $config = $c['amqp-config'];
-        return new \PhpAmqpLib\Connection\AMQPConnection($config->host, $config->port, $config->user, $config->pass);
+        $slept = 0;
+        while (1) {
+            try {
+                $conn = new \PhpAmqpLib\Connection\AMQPConnection($config->host, $config->port, $config->user, $config->pass);
+                return $conn;
+            } catch (\PhpAmqpLib\Exception\AMQPRuntimeException $exception) {
+                if ($slept >= $config->timeout) {
+                    throw $exception;
+                }
+                echo "$slept\n";
+                $slept += 1;
+                sleep(1);
+            }
+        }
     },
-    "queue-driver" => function($c) {
+    "queue-driver" => function ($c) {
         return new \BunnyAcme\Queue\Driver\AMQPDriver($c);
     },
-    "queue-manager" => function($c) {
+    "queue-manager" => function ($c) {
         return new \BunnyAcme\Queue\QueueManager($c);
     },
-    "queues-list" => function($c) {
-        return array(      
+    "queues-list" => function ($c) {
+        return array(
             'sleep' => array(
                 new \BunnyAcme\Queue\Workers\SleepyWorker($c)
             )
         );
     },
-     
+
 );
         
